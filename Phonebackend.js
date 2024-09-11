0879a31f38949fda69b7c277878298b0fdafa82c
Jc9697 <124229632+Jc9697@users.noreply.github.com>
2024-09-11 10:53:34 -0500

    Second commit

--- Phonebackend.js
+++ Phonebackend.js
@@ -1,1 +1,63 @@
-console.log('Hello')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = 
+
+[
+    { 
+      id: "1",
+      name: "Arto Hellas", 
+      number: "040-123456"
+    },
+    { 
+      id: "2",
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
+    },
+    { 
+      id: "3",
+      name: "Dan Abramov", 
+      number: "12-43-234345"
+    },
+    { 
+      id: "4",
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
+    }
+]
+
+app.get('/info',(request,response) => {
+    response.send(`<p>Phonebook has info for ${persons.length}
+    people <p>${Date()}</p></p>`)
+})
+
+app.get('/api/persons',(request,response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id',(request,response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if(person) {
+        response.json(person)
+    }
+    else {
+        response.status(204).end()
+    }
+})
+
+app.delete('/api/persons/:id',(request,response) => {
+  const id = request.params.id
+  if(id) {
+    persons = persons.filter(person => person.id !== id)
+  }
+  else {
+    response.status(404).end() 
+  }
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
